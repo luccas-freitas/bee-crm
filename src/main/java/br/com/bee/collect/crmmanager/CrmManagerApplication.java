e83d4e5ced09ee2096306e54c4ce9f18f73ff2aa
Luccas Freitas <luccas-freitas>
2022-08-01 22:06:03 -0300

    First commit

---
+++ src/main/java/br/com/bee/collect/crmmanager/CrmManagerApplication.java
@@ -0,0 +1,13 @@
+package br.com.bee.collect.crmmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/bee/collect/crmmanager/CrmManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bee.collect.crmmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
