8aef848feaad418b4a71da6cadd861fcf6408e7c
Luccas Freitas <luccas-freitas>
2022-08-04 21:22:53 -0300

    add navbar and footer

--- src/main/java/br/com/bee/collect/crmmanager/config/SecurityConfiguration.java
+++ src/main/java/br/com/bee/collect/crmmanager/config/SecurityConfiguration.java
@@ -4,18 +4,28 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests()
+        http.authorizeRequests()
                 .antMatchers("/").authenticated()
                 .antMatchers("/saved").authenticated()
                 .and()
                 .formLogin()
+                    .loginPage("/login")
+                    //.usernameParameter("email")
+                    .defaultSuccessUrl("/")
+                    .permitAll()
                 .and()
-                .logout().logoutSuccessUrl("/login")
+                .logout()
+                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                    .logoutSuccessUrl("/login")
+                    .permitAll()
+                .and()
+                .rememberMe()
                 .and()
                 .csrf().disable()
                 .httpBasic();
